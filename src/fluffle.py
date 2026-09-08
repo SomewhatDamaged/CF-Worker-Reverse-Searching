a5b4b5b90cb2db3e2ded236aa18e2cb7c9794eb7
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 06:03:15 +1000

    Testing 2

--- src/fluffle.py
+++ src/fluffle.py
@@ -12,20 +12,16 @@ async def fluffle(request: Request, env: Any) -> Response:
     url = URL(request.url)
     queries = url.query
     image_url = queries["url"]
-    image_config = {
-        "width": 1000,
-        "height": 1000,
-        "fit": "scale-down",
-        "format": "jpeg",
-        "quality": 85
-    }
-    js_image_opts = Object.fromEntries(Object.entries(image_config))
-    js_cf_block = Object.fromEntries(Object.entries({
-        "image": js_image_opts
-    }))
-    fetch_options = Object.fromEntries(Object.entries({
-        "cf": js_cf_block
-    }))
+    fetch_options = Object.new()
+    js_cf_block = Object.new()
+    js_image_opts = Object.new()
+    js_image_opts.width = 1000
+    js_image_opts.height = 1000
+    js_image_opts.fit = "scale-down"
+    js_image_opts.format = "jpeg"
+    js_image_opts.quality = 85
+    js_cf_block.image = js_image_opts
+    fetch_options.cf = js_cf_block
     image_data = await fetch(image_url, fetch_options)
     raise ValueError(f"Resized Header: {image_data.headers.get('cf-resized')}")
     array_buffer = await image_data.arrayBuffer()
