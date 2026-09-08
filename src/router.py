766b92d90345c0be6e80d6de11bdf6fe9abb4d2a
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 06:30:35 +1000

    Cleanup

--- src/router.py
+++ src/router.py
@@ -2,6 +2,8 @@ from fluffle import fluffle
 from urllib.parse import urlparse
 from workers import WorkerEntrypoint, Response, Request
 import traceback
+from defs import *
+from json import loads
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request: Request) -> Response:
@@ -16,9 +18,29 @@ class Default(WorkerEntrypoint):
                 # ...
             if request.method == "POST":
                 if pathname.startswith("/fluffle"):
+                    response = await self.authenticate(request, "reverse")
+                    if isinstance(response, Response):
+                        return response
                     return await fluffle(request, self.env)
                 # ...
             return Response(status=404)
         except Exception:
             headers = {"content-type": "text/plain;charset=UTF-8"}
             return Response(f"Traceback: {traceback.format_exc()}", headers=headers, status=500)
+
+    async def authenticate(self, request: Request, endpoint_name: str) -> Union[Response,str]:
+        headers = dict(request.headers)
+        if "authorization" not in headers.keys():
+            return Response('{"error": "Missing \'authorization\' parameter"}', headers=json_header, status=400)
+        if not headers["authorization"].startswith("Bearer "):
+            return Response('{"error": "Bad \'authorization\' parameter"}', headers=json_header, status=400)
+        key = headers["authorization"].split(" ")[1]
+        key_data = str(await self.env.KEYS.get(key)).strip()
+        if not key_data.startswith("{"):
+            return Response('{"error": "Invalid \'key\' parameter"}', headers=json_header, status=401)
+        authorized_data = loads(key_data)
+        if authorized_data is None:
+            return Response('{"error": "Error parsing key data from KV storage"}', headers=json_header, status=500)
+        if endpoint_name not in authorized_data["access"]:
+            return Response('{"error": "You do not have access to this endpoint"}', headers=json_header, status=403)
+        return key
