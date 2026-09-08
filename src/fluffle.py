85b060ffce97592469674a8d8e1aeb39c59b620e
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 01:52:50 +1000

    Fuck aiohttp. Fuck httpx. WE ARE GOING IN RAW JS!

--- src/fluffle.py
+++ src/fluffle.py
@@ -1,43 +1,32 @@
 from urllib.parse import urlsplit, parse_qs
-from js import fetch as pyfetch
+import js
 from workers import Response, Request
 import json
-import io
 from defs import *
+from pyodide.ffi import to_js
 
 async def fluffle(request: Request) -> Response:
-    import httpx
-    from httpx_socks import AsyncProxyTransport
     url = urlsplit(request.url)
     queries = parse_qs(url.query)
     if "url" not in queries.keys():
         return Response('{"error": "Missing \'url\' parameter"}', headers=json_header, status=400)
-    image_data = await pyfetch(queries["url"][0])
+    image_data = await js.fetch(queries["url"][0])
     content_type = image_data.headers.get("content-type")
     blob = await image_data.blob()
-    array_buffer = await blob.arrayBuffer()
-    python_bytes = array_buffer.to_py().tobytes()
-    buffer = io.BytesIO(python_bytes)
-    headers = {
-        "user-agent": user_agent
+    form_data = js.FormData.new()
+    form_data.append("file", blob, "image." + content_type.split("/")[1])
+    form_data.append("limit", "8")
+    options = {
+        "method": "POST",
+        "headers": {
+            "User-Agent": user_agent
+        },
+        "body": form_data
     }
-    files = {
-        "file": ("image." + content_type.split("/")[1], buffer.getvalue(), content_type)
-    }
-    data = {
-        "limit": "8"
-    }
-    transport = AsyncProxyTransport.from_url('socks5://localhost:1080')
-    async with httpx.AsyncClient(headers=headers, transport=transport, http1=True, http2=False) as client:
-        response = await client.post(
-            'https://api.fluffle.xyz/exact-search-by-file',
-            data=data,
-            files=files
-        )
-        result = response.json()
+    js_response = await js.fetch(url, to_js(options))
+    result = json.loads(await js_response.text())
     result = {
         "success": True,
         "data": result,
-        "user-agent": user_agent,
     }
     return Response(json.dumps(result), headers=json_header, status=200)
