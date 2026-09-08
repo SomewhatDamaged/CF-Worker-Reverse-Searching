d0be4eaee297c07801818e60d4860b59103b738e
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 03:53:06 +1000

    Fuck aiohttp. Fuck httpx. WE ARE GOING IN RAW JS! Fuck this, lemme smash. RPC?

--- src/fluffle.py
+++ src/fluffle.py
@@ -4,22 +4,17 @@ from js import fetch
 from workers import Response, Request
 import json
 from defs import *
-from pyodide.ffi import to_js
 from typing import Any
-from pathlib import Path
 
 async def fluffle(request: Request, env: Any) -> Response:
     url = urlsplit(request.url)
     queries = parse_qs(url.query)
     image_data = await fetch(queries["url"][0])
     content_type = image_data.headers.get("content-type", "image/png")
-    blob = await image_data.blob()
-    data = []
-    current_dir = os.getcwd()
-    data.append(f"{current_dir = }")
+    array_buffer = await image_data.arrayBuffer()
     rpc = env.RPC
     js_result = await rpc.search(
-        to_js(blob),
+        array_buffer,
         "8",
         content_type
     )
