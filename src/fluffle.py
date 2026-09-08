530b18e02a974c3afaf3fa86532b3e1c7550b809
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 04:52:35 +1000

    Some bugfix 5

--- src/fluffle.py
+++ src/fluffle.py
@@ -9,7 +9,7 @@ from yarl import URL
 async def fluffle(request: Request, env: Any) -> Response:
     url = URL(request.url)
     queries = url.query
-    image_url = queries["url"][0]
+    image_url = queries["url"]
     raise ValueError(f"{image_url = }")
     headers = {"User-Agent": user_agent, "content-type": "image/*"}
     image_data = await fetch(image_url)
