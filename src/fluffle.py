9d075e2eb1f1f8282dd9aa93accdf0078982adc1
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 04:45:54 +1000

    Some bugfix 4

--- src/fluffle.py
+++ src/fluffle.py
@@ -9,10 +9,10 @@ async def fluffle(request: Request, env: Any) -> Response:
     url = urlsplit(request.url)
     queries = parse_qs(url.query)
     image_url = queries["url"][0]
+    raise ValueError(f"{image_url = }")
     headers = {"User-Agent": user_agent, "content-type": "image/*"}
     image_data = await fetch(image_url)
     content_type = image_data.headers.get("content-type", None)
-    raise ValueError(f"{content_type = }")
     if content_type is None:
         content_type = "image/" + url.path.rsplit(".", 1)[1]
     array_buffer = await image_data.arrayBuffer()
