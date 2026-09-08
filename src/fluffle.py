7f2327ad192ed96eb08ce094da65908d6c70708e
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 06:05:10 +1000

    Testing 3

--- src/fluffle.py
+++ src/fluffle.py
@@ -6,8 +6,6 @@ from defs import *
 from typing import Any
 from yarl import URL
 
-MAX_BYTES = 4 * 1024 * 1024
-
 async def fluffle(request: Request, env: Any) -> Response:
     url = URL(request.url)
     queries = url.query
@@ -15,15 +13,14 @@ async def fluffle(request: Request, env: Any) -> Response:
     fetch_options = Object.new()
     js_cf_block = Object.new()
     js_image_opts = Object.new()
-    js_image_opts.width = 1000
-    js_image_opts.height = 1000
+    js_image_opts.width = 2000
+    js_image_opts.height = 2000
     js_image_opts.fit = "scale-down"
     js_image_opts.format = "jpeg"
     js_image_opts.quality = 85
     js_cf_block.image = js_image_opts
     fetch_options.cf = js_cf_block
     image_data = await fetch(image_url, fetch_options)
-    raise ValueError(f"Resized Header: {image_data.headers.get('cf-resized')}")
     array_buffer = await image_data.arrayBuffer()
 
     js_result = await env.RPC.search(
