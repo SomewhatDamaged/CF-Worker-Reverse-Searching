3674074a7020fd45dc2c35dc1230ee6c169257bd
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-09-09 05:12:09 +1000

    Some bugfix 9

--- src/fluffle.py
+++ src/fluffle.py
@@ -10,8 +10,7 @@ async def fluffle(request: Request, env: Any) -> Response:
     url = URL(request.url)
     queries = url.query
     image_url = queries["url"]
-    headers = {"User-Agent": user_agent, "content-type": "image/*"}
-    image_data = await fetch(image_url, headers=headers)
+    image_data = await fetch(image_url)
     content_type = image_data.headers.get("content-type", None)
     if content_type is None:
         content_type = "image/" + url.path.rsplit(".", 1)[1]
